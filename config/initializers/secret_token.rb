4100b99346f2917ca2af4720b4fcedf9d0e572d9
Lê Hoàng Phương <herophuong93@gmail.com>
2013-07-05 09:46:21 +0700

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drs::Application.config.secret_token = 'e136b1118b9b176428ae230d341c78f9060729777f1630225c788017d45966f3b212b74590438277865ecb32bb80ecb4bb5e7fd46040549ba68a86357aa32b26'

c5819a71f68ca4e073b6f5b17136c521307f4fde
Lê Hoàng Phương <herophuong93@gmail.com>
2013-07-05 10:48:18 +0700

    added factories, admin attrib for group

---
+++ spec/factories.rb
@@ -0,0 +1,33 @@
+FactoryGirl.define do
+    factory :user do
+        sequence(:name) { |n| "Person #{n}" }
+        sequence(:email) { |n| "person_#{n}@framgia.com"}
+        password    "foobar"
+        password_confirmation "foobar"
+        
+        after_build {|user| user.send(:initialize_state_machines, :dynamic => :force)}
+#         group
+        factory :manager do
+            manager true
+        end
+    end
+    
+    factory :group do
+        sequence(:name) { |n| "Group #{n}" }
+        
+        factory :group_admin do
+            admin true
+        end
+    end
+    
+    factory :report do
+        content "Lorem Ipsum"
+#         reporttype
+#         user
+    end
+    
+    factory :report_type do
+        title "Lorem Ipsum"
+        guide "Lorem Ipsum"
+    end
+end
